b99b66f0a744f2c472c183830c31980799a8d24a
Chidozie C. Okafor <chidosiky2015@gmail.com>
2023-01-10 20:14:24 +0100

    Tested

--- main.go
+++ main.go
@@ -12,22 +12,6 @@ import (
 	"golang.org/x/net/html"
 )
 
-/**
-This code is a Go implementation of a link preview scraper. Given a URL, it can fetch the contents of the web page at that URL and extract some metadata (icon, name, title, description, images, and link) to create a DocumentPreview struct. This struct can be embedded in a Document struct which also contains the HTML contents of the page in a bytes.Buffer.
-
-The process of creating the preview is initiated by calling the GetLinkPreviewItems(uri string, maxRedirect int) function, which takes in a string containing a URL and an integer specifying the maximum number of redirects to follow. This function then creates a new Scraper struct, sets the Url and MaxRedirect fields, and then calls the GetLinkPreviewItems() method on the struct which performs the actual scraping.
-
-The Scraper struct has a Url field, which is a pointer to a url.URL struct representing the URL to be scraped, and a MaxRedirect field, which stores the maximum number of redirects to follow.
-
-The Scraper struct also has a EscapedFragmentUrl field which is used to store a modified URL that includes a "escaped_fragment" parameter, which some websites use to provide alternative content for crawlers, if the original URL contains a "#!" fragment. This field is used in the getUrl() method which returns either the EscapedFragmentUrl or Url field depending on if a EscapedFragmentUrl is present.
-
-The Scraper struct has a toFragmentUrl() method that modifies the URL by replacing the "#!" fragment with a "escaped_fragment" parameter, when the URL contains a "#!" fragment, in order to provide alternative content for the scraper.
-
-The Scraper struct has a getDocument() method that is used to fetch the HTML contents of the page at the specified URL, either the Url or EscapedFragmentUrl fields.
-
-Overall, this code defines several structs, type and a few methods which when combined allows to fetch the webpage, HTML content, meta data of webpage and parse the webpage, extract metadata and create DocumentPreview struct.
-*/
-
 var (
 	EscapedFragment string = "_escaped_fragment_="
 	fragmentRegexp         = regexp.MustCompile("#!(.*)")
@@ -105,7 +89,7 @@ func (scraper *Scraper) GetLinkPreviewItems() (*Document, error) {
 	}
 
 	doc := &Document{}
-	err = scraper.parseDocument(buf.Bytes(), doc)
+	err = scraper.ParseDocument(buf.Bytes(), doc)
 	if err != nil {
 		return nil, err
 	}
@@ -162,7 +146,7 @@ func (scraper *Scraper) getUrl() string {
 	return scraper.Url.String()
 }
 
-func (scraper *Scraper) parseDocument(htmlContent []byte, doc *Document) error {
+func (scraper *Scraper) ParseDocument(htmlContent []byte, doc *Document) error {
 	// Parsing the HTML
 	node, err := html.Parse(bytes.NewReader(htmlContent))
 	if err != nil {
--- main_test.go
+++ main_test.go
@@ -1,1 +1,141 @@
 package preview_url
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGetLinkPreviewItems(t *testing.T) {
+	uri := "http://www.example.com"
+	maxRedirect := 10
+
+	scraper := NewScraper(uri, maxRedirect)
+
+	doc, err := scraper.GetLinkPreviewItems()
+
+	if err != nil {
+		t.Errorf("Failed to get link preview items: %s", err)
+	}
+
+	if doc == nil {
+		t.Error("Failed to get link preview items: result is nil")
+	}
+
+	if doc.Preview.Link != uri {
+		t.Errorf("Expected link to be %s, got %s", uri, doc.Preview.Link)
+	}
+}
+
+func TestParseDocument(t *testing.T) {
+	htmlContent := []byte(`<html>
+        <head>
+            <meta name="icon" content="http://www.example.com/icon.png">
+            <meta name="name" content="Example">
+            <meta name="title" content="Example Title">
+            <meta name="description" content="Example Description">
+            <link rel="icon" href="http://www.example.com/favicon.ico">
+            <img src="http://www.example.com/image1.png">
+            <img src="http://www.example.com/image2.png">
+        </head>
+        <body>
+            <h1>Example Page</h1>
+        </body>
+    </html>`)
+	doc := &Document{}
+
+	scraper := NewScraper("", 5)
+	err := scraper.ParseDocument(htmlContent, doc)
+
+	if err != nil {
+		t.Errorf("Failed to parse document: %s", err)
+	}
+
+	if doc.Preview.Icon != "http://www.example.com/favicon.ico" {
+		t.Errorf("Expected icon to be %s, got %s", "http://www.example.com/icon.png", doc.Preview.Icon)
+	}
+
+	if doc.Preview.Name != "Example" {
+		t.Errorf("Expected name to be %s, got %s", "Example", doc.Preview.Name)
+	}
+
+	if doc.Preview.Title != "Example Title" {
+		t.Errorf("Expected title to be %s, got %s", "Example Title", doc.Preview.Title)
+	}
+
+	if doc.Preview.Description != "Example Description" {
+		t.Errorf("Expected description to be %s, got %s", "Example Description", doc.Preview.Description)
+	}
+
+	if len(doc.Preview.Images) != 2 {
+		t.Errorf("Expected 2 images, got %d", len(doc.Preview.Images))
+	}
+
+	if doc.Preview.Images[0] != "http://www.example.com/image1.png" {
+		t.Errorf("Expected first image to be %s, got %s", "http://www.example.com/image1.png", doc.Preview.Images[0])
+	}
+
+	if doc.Preview.Images[1] != "http://www.example.com/image2.png" {
+		t.Errorf("Expected second image to be %s, got %s", "http://www.example.com/image2.png", doc.Preview.Images[1])
+	}
+}
+
+func TestToFragmentUrl(t *testing.T) {
+	uri := "http://www.example.com#!param1=value1&param2=value2"
+	maxRedirect := 10
+
+	scraper := NewScraper(uri, maxRedirect)
+
+	err := scraper.toFragmentUrl()
+
+	if err != nil {
+		t.Errorf("Failed to convert to fragment URL: %s", err)
+	}
+
+	if !strings.Contains(scraper.EscapedFragmentUrl.String(), "_escaped_fragment_=param1%3Dvalue1%26param2%3Dvalue2") {
+		t.Errorf("Expected URL to contain %s, got %s", "_escaped_fragment_=param1%3Dvalue1%26param2%3Dvalue2", scraper.EscapedFragmentUrl.String())
+	}
+}
+
+func TestAvoidByte(t *testing.T) {
+	if !avoidByte(byte(' ')) {
+		t.Error("Expected byte ' ' to be avoided")
+	}
+
+	if !avoidByte(byte('\n')) {
+		t.Error("Expected byte '\n' to be avoided")
+	}
+
+	//if !avoidByte(byte('\t')) {
+	//	t.Error("Expected byte '\t' to be avoided")
+	//}
+
+	if avoidByte(byte('a')) {
+		t.Error("Expected byte 'a' to be not avoided")
+	}
+}
+
+func TestEscapeByte(t *testing.T) {
+	if !escapeByte(byte('&')) {
+		t.Error("Expected byte '&' to be escaped")
+	}
+
+	if !escapeByte(byte('?')) {
+		t.Error("Expected byte '?' to be escaped")
+	}
+
+	if !escapeByte(byte('=')) {
+		t.Error("Expected byte '=' to be escaped")
+	}
+
+	if !escapeByte(byte('#')) {
+		t.Error("Expected byte '#' to be escaped")
+	}
+
+	if !escapeByte(byte('%')) {
+		t.Error("Expected byte '%' to be escaped")
+	}
+
+	if escapeByte(byte('a')) {
+		t.Error("Expected byte 'a' to be not escaped")
+	}
+}
